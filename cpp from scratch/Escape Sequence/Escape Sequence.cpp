d1dcd1f093ef3b46acd69717d8a74671d7463e0a
Omarabdalgwad <ahlawyomar95@gmail.com>
2025-11-02 03:59:01 +0200

    Add files via upload

---
+++ cpp from scratch/Escape Sequence/Escape Sequence.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "welcome to\b \t c++\n";
+    std::cout << "your name\rOmar \a \n";
+    std::cout << "\"Omar\"" << std::endl; 
+    std::cout << "\'Omar\'" << std::endl;
+    std::cout << "\\Omar\\" << std::endl;
+   
+    return 0;
+}
---
+++ cpp from scratch/first program/first program.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+int main()
+{
+	std::cout << "Welcome to c++";
+	std::cout << "your name";
+
+	return 0;
+}
